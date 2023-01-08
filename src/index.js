0f20567986e9dcf498c5121184176777317d5e4d
Vlada Lebron <peinfrantic@gmail.com>
2023-01-08 18:08:19 +0300

    fix index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import readlineSync from 'readline-sync';
-import { greetingBrainEven, startBrainEven } from './games/brain-even.js';
-import { greetingBrainCalc, startBrainCalc } from './games/brain-calc.js';
-import { greetingBrainGcd, startBrainGcd } from './games/brain-gcd.js';
-import { greetingBrainPrime, startBrainPrime } from './games/brain-prime.js';
-import { greetingBrainProgression, startBrainProgression } from './games/brain-progression.js';
 
+const getName = () => readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
+const userName = getName();
 export const getRandomNumber = () => Math.round(Math.random() * 100);
 export const askQuestion = (expression) => {
   const question = expression;
@@ -22,55 +19,13 @@ export const test = (num1, num2) => {
   return false;
 };
 
-export default function startGame(game) {
-  console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
+export const greeting = () => {
   console.log(`Hello, ${userName}!`);
+};
 
-  const gameName = game;
-
-  switch (gameName) {
-    case 'brain-even':
-      greetingBrainEven();
-      break;
-    case 'brain-calc':
-      greetingBrainCalc();
-      break;
-    case 'brain-gcd':
-      greetingBrainGcd();
-      break;
-    case 'brain-prime':
-      greetingBrainPrime();
-      break;
-    case 'brain-progression':
-      greetingBrainProgression();
-      break;
-    default:
-      throw new Error(`Unknown game: ${gameName}!`);
-  }
-
-  let result;
-
+export const startGame = (startRound) => {
   for (let i = 0; i < 3; i += 1) {
-    switch (gameName) {
-      case 'brain-even':
-        result = startBrainEven();
-        break;
-      case 'brain-calc':
-        result = startBrainCalc();
-        break;
-      case 'brain-gcd':
-        result = startBrainGcd();
-        break;
-      case 'brain-prime':
-        result = startBrainPrime();
-        break;
-      case 'brain-progression':
-        result = startBrainProgression();
-        break;
-      default:
-        throw new Error(`Unknown game: ${gameName}!`);
-    }
+    const result = startRound();
 
     if (!result) {
       console.log(`Let's try again, ${userName}!`);
@@ -78,4 +33,4 @@ export default function startGame(game) {
     }
   }
   console.log(`Congratulations, ${userName}!`);
-}
+};
