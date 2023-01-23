6c15365b0e323bf65f47586a58dca429d2f5f216
Vlada Lebron <peinfrantic@gmail.com>
2023-01-23 15:00:53 +0300

    fix brain-prime

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -9,7 +9,7 @@ const isPrime = (number) => {
   if (num === 2) {
     return true;
   }
-  for (let i = 3; i < num / 2; i += 1) {
+  for (let i = 2; i < num / 2; i += 1) {
     if (num % i === 0) {
       return false;
     }
